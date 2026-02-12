49f3af493ca599f6a6f8e3e8fc5124520b6c44ad
Erick <202301092@iacademy.edu.ph>
2026-02-13 00:35:33 +0800

    Fix build configuration

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,13 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8080',
-        changeOrigin: true,
-      }
-    }
+    port: 3000
   },
   build: {
     outDir: 'dist',
-    sourcemap: false,
+    emptyOutDir: true
   }
 })
